8eb0dc559e13a3332aabdbc784162f60ac9a9406
AyesoroFemi <emmafem92@gmail.com>
2022-11-15 11:29:06 +0100

    dom class

---
+++ container/Assignment/main.js
@@ -0,0 +1,11 @@
+var form = document.getElementById("addForm");
+var itemList = document.getElementById("items");
+
+
+form.addEventListener("submit", addItem);
+// Delete event
+itemList.addEventListener("click", removeItem);
+
+function addItem(){
+    
+}
